555e08caad0709aa9a182908984401fbbed64d69
yandld <b47088@freescale.com>
2013-12-01 20:43:32 +0800

    Fix OV7620 bug
     -LCD is not initilazied

--- Projects/CHKAtom_Examples/20-OV7620(GPIO DMA I2C)/src/main.c
+++ Projects/CHKAtom_Examples/20-OV7620(GPIO DMA I2C)/src/main.c
@@ -35,9 +35,36 @@
 //utilities:      软件模块支持库(部分实验用到)
 //drivers:        固件库源码
 //use:            用户代码 包含mian函数和 中断服务程序 
+//LCD Devices
+#include "spilcd.h"  //物理层 LCD驱动
+#include "chgui.h"         //CHGUI基本组件
+#include "chgui_char.h"    //CHGUI字符显示组件
+#include "chgui_bmp.h"     //CHGUI BMP图片解码组件
 
 
 #include "ov7620.h"
+CHGUI_CtrlOperation_TypeDef gILI9320_OperationStrcut1 = 
+{
+	SPILCD_Init,        //底层函数与CHGUI无关 LCD_Init的实现在 LCD_CHK60EVB.c中 下同
+	NULL,
+	SPILCD_DrawPoint,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	SPILCD_Fill,
+	NULL,
+};
+
+//CHGUI初始化结构
+CHGUI_InitTypeDef CHGUI_InitStruct1 = 
+{
+    "CHK60", //设备名称
+    0,      //设备号 如不明确填0
+    SPILCD_W, //LCD X方向最大值 像素
+    SPILCD_H, //LCD Y方向最大值 像素
+    &gILI9320_OperationStrcut1,
+};
 void I2C_ConfigOV7620(void);
 void DisplayImage_WithSPILCD(uint16_t x,uint16_t y,uint8_t mode);
 uint8_t StrBuffer[30];
@@ -50,8 +77,16 @@ int main(void)
     //初始化系统时钟 使用外部50M晶振 PLL倍频到100M
     SystemClockSetup(ClockSource_EX50M,CoreClock_100M);
     DelayInit();
-    LED_Init(LED_PinLookup_CHKATOM, kNumOfLED);
-    KBI_Init(KBI_PinLookup_CHKATOM, kNumOfKEY);
+	   //初始化GUI
+    GUI_Init(&CHGUI_InitStruct1);
+    //设置背景色为黑色
+    GUI_SetBkColor(BLACK);
+    //普通字符显示模式
+    GUI_SetTextMode(GUI_TEXTMODE_NORMAL);
+    //设置前景色为红色
+    GUI_SetColor(LGRAY);
+    //LED_Init(LED_PinLookup_CHKATOM, kNumOfLED);
+    //KBI_Init(KBI_PinLookup_CHKATOM, kNumOfKEY);
     UART_DebugPortInit(UART4_RX_C14_TX_C15, 115200);
     UART_printf("HelloWorld\r\n");
     DisplayCPUInfo();
